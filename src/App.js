c604fa8ddf9a4c357958483f0e584cb01e4d7432
aromanchev <aromanchev@businessolver.com>
2022-05-14 18:06:40 +0300

    change app js

--- src/App.js
+++ src/App.js
@@ -95,10 +95,7 @@ const App = () => {
     return (
         <div className="App">
             <h1>
-                <a href='https://jetblackcodes.medium.com/%D0%BF%D0%BE%D0%BB%D0%BD%D1%8B%D0%B9-%D1%80%D0%B0%D0%B7%D0%B1%D0%BE%D1%80-ecmascript-2015-2016-2017-2018-2019-2020-2021-3e7263924a5f'>
-                    ECMAScript version:
-                </a>
-                {' '}{activeVersion}
+                ECMAScript version: {activeVersion}
             </h1>
             <button onClick={handleEs6}>ES6</button>
             <button onClick={handleEs7}>ES7</button>
