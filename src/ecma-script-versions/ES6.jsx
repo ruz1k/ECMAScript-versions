718998be10300f1ca6c2f94ee932af443bac5a3c
ruz1k <outpac1488@yandex.ru>
2022-06-25 15:06:18 +0300

    asynciterator add

--- src/ecma-script-versions/ES6.jsx
+++ src/ecma-script-versions/ES6.jsx
@@ -1,26 +1,81 @@
-import React from 'react'
+import React from "react";
 
 export const ES6 = () => {
+  /// Symbol iterator
+  let range = {
+    from: 1,
+    to: 5,
+  };
+  range[Symbol.iterator] = function () {
+    let current = this.from;
+    let last = this.to;
+    return {
+      next() {
+        if (current <= last) {
+          return {
+            done: false,
+            value: current++,
+          };
+        } else {
+          return {
+            done: true,
+          };
+        }
+      },
+    };
+  };
 
-    /// Generator
-    /// function* || function *funcName()
-    function* generateSequence() {
-        yield 1;
-        yield 2;
-        return 3;
+  for (let num of range) {
+    console.log(num);
+  }
+
+  /// Symbol iterator async
+  const asyncIterator = {
+    from: 1,
+    to: 5,
+    [Symbol.asyncIterator]() {
+      return {
+        current: this.from,
+        last: this.to,
+
+        async next() {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+
+          if (this.current <= this.last) {
+            return { done: false, value: this.current++ };
+          } else {
+            return { done: true };
+          }
+        },
+      };
+    },
+  };
+
+  (async () => {
+    for await (let value of asyncIterator) {
+      console.log("asyncIterator value: ", value);
     }
+  })();
 
-    const generator = generateSequence();
+  /// Generator
+  /// function* || function *funcName()
+  function* generateSequence() {
+    yield 1;
+    yield 2;
+    return 3;
+  }
 
-    const generator1 = generator.next();
-    const generator2 = generator.next();
-    const generator3 = generator.next();
-    console.log('Generator', generator1, generator2, generator3)
+  const generator = generateSequence();
 
-    return (
-        <div style={{display: 'flex'}}>
-            <p>ES6:</p>
-            <p>Generator</p>
-        </div>
-    )
-}
+  const generator1 = generator.next();
+  const generator2 = generator.next();
+  const generator3 = generator.next();
+  console.log("Generator", generator1, generator2, generator3);
+
+  return (
+    <div style={{ display: "flex" }}>
+      <p>ES6:</p>
+      <p>Generator</p>
+    </div>
+  );
+};
